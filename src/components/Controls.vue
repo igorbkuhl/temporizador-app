fbdecb8380ed5d1c6bd4fa0f24fcdd6c572105dc
igorbkuhl <igorb.kuhl@gmail.com>
2023-08-07 10:58:15 -0300

    Adicionei os botões do contador.

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,25 +1,68 @@
 <template>
-    <PlayButton />
-    <PauseButton />
-    <StopButton />
+    <button @click="changeStatus('running')" :disabled="isRunning">
+        <span class="icon">
+            <i class="fas fa-play"></i>
+        </span>
+        <span>Play</span>
+    </button>
+    <button @click="changeStatus('paused')" :disabled="isPaused">
+        <span class="icon">
+            <i class="fas fa-pause"></i>
+        </span>
+        <span>Pause</span>
+    </button>
+    <button @click="changeStatus('stopped')" :disabled="!isRunning && !isPaused">
+        <span class="icon">
+            <i class="fas fa-stop"></i>
+        </span>
+        <span>Stop</span>
+    </button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import PlayButton from '@/components/PlayButton.vue'
-import PauseButton from '@/components/PauseButton.vue'
-import StopButton from '@/components/StopButton.vue'
 
 export default defineComponent({
     name: 'Controls',
-    components: {
-        PlayButton,
-        PauseButton,
-        StopButton
+    emits: ['play', 'pause', 'stop'],
+    data() {
+        return {
+            isRunning: false,
+            isPaused: false
+        }
+    },
+    methods: {
+        changeStatus(status: string) {
+            console.log("O novo estado do computador é " + status);
+            switch(status) {
+                case 'running':
+                    this.isRunning = true
+                    this.isPaused = false
+                    break;
+                case 'paused':
+                    this.isRunning = false
+                    this.isPaused = true
+                    break;
+                case 'stopped':
+                    this.isRunning = false
+                    this.isPaused = false
+                    break;
+                default:
+                    // throw emite um erro propositalmente
+                    throw new Error("Estado desconhecido: " + status);
+            }
+        }
     }
-})
+});
 </script>
 
 <style scoped>
+button {
+    cursor: pointer;
+    padding: 20px 40px;
+}
 
+button span {
+    margin: 5px;
+}
 </style>
