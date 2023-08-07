dc4d253f630d11fbea52756956ae7b9e9fc02aa5
igorbkuhl <igorb.kuhl@gmail.com>
2023-08-07 11:00:54 -0300

    Arquivo Message adicionado.

---
+++ src/components/Message.vue
@@ -0,0 +1,17 @@
+<template>
+   <span>
+        <slot></slot>
+   </span> 
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    name: 'Message'
+});
+</script>
+
+<style scoped>
+
+</style>
