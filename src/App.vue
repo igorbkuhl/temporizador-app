35dbdd485b5bab3d470dc04c5e930166f0e90c35
igorbkuhl <igorb.kuhl@gmail.com>
2023-08-07 10:54:58 -0300

    Adicionei estilos para a página.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <router-view/>
+  <center><router-view/></center>
 </template>
 
 <script lang="ts">
 
 </script>
 
-<style scoped>
-
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center;
+  color: #2c3e50; */
+}
 </style>
