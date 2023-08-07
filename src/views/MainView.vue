00ae343ac246bb16566ce9a22633f397c0451be9
igorbkuhl <igorb.kuhl@gmail.com>
2023-08-07 11:03:52 -0300

    Arquivo Message importado e algumas adições em HTML e CSS.

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -3,8 +3,11 @@
   <aside><!-- Conteúdo secundário/lateral do site --></aside>
   <main>
     <!-- Conteúdo principal do site -->
+    <h1>CONTADOR</h1>
+    <h4>Instituto Evoluir</h4>
     <!-- 03. Utilizar o componente -->
     <MadCounter />
+    <Message>{{ message }}</Message>
   </main>
 </template>
 
@@ -13,16 +16,28 @@ import { defineComponent } from 'vue';
 
 // 01. Importar o arquivo do componente
 import MadCounter from '@/components/MadCounter.vue'
+import Message from '@/components/Message.vue'
 
 export default defineComponent({
   name: 'MainView',
+  props: {
+    message: String,
+    title: String
+  },
   // 02. Listar os componentes que serão utilizados
   components: {
+    Message,
     MadCounter
   },
 });
 </script>
 
 <style scoped>
-
+* {
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+h1, h4 {
+  display: flex;
+  justify-content: center;
+}
 </style>
